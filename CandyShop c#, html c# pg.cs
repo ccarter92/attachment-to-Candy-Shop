0b2f64e613709c7c30a6582f9970ab673390634f
ccarter92 <54338971+ccarter92@users.noreply.github.com>
2019-10-01 10:58:30 -0500

    Add files via upload

---
+++ CandyShop c#, html c# pg.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+
+namespace FirstApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}    class CandyShop
+{
+   
+
+    public string _Candies { get; set; }
+    public string _Chips { get; set; }
+    
+    
+
+}
+
+
